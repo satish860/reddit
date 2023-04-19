3256255d48683467593f0b8ce2d39d867a4cd597
Satish Venkatakrishnan <satish860@gmail.com>
2023-04-19 18:00:42 +0530

    Added the Dummy Navbar component to the app

--- src/pages/components/Layout.tsx
+++ src/pages/components/Layout.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Navbar from "./Navbar/Navbar";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return <main>{children}</main>;
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
 };
 export default Layout;
