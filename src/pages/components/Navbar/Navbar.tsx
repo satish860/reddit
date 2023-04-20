40d5be05cd4dc7b8c36eaeb33a009c9ebb955af2
Satish Venkatakrishnan <satish860@gmail.com>
2023-04-20 10:05:51 +0530

    Menu Added with the Logo and Text

--- src/pages/components/Navbar/Navbar.tsx
+++ src/pages/components/Navbar/Navbar.tsx
@@ -1,8 +1,26 @@
+import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 
 type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
-  return <div>Nav Bar</div>;
+  return (
+    <Flex bg="white" h="44px" padding="6px 12px">
+      <Flex align="center" cursor={"pointer"}>
+        <Image
+          src="/images/redditFace.svg"
+          width={{ base: "24px", md: "32px" }}
+          mr={{ base: "0px", md: "2px" }}
+          alt="logo"
+        />
+        <Image
+          src="/images/redditText.svg"
+          display={{ base: "none", md: "unset" }}
+          height="46px"
+          alt="Text"
+        />
+      </Flex>
+    </Flex>
+  );
 };
 export default Navbar;
